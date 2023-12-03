eef61546ddb5a6139ea275d63a29b50466041d39
Adomas Baliuka <A.Baliuka@physik.uni-muenchen.de>
2023-12-04 00:20:00 +0100

    Remove Mathlib dependency

--- lakefile.lean
+++ lakefile.lean
@@ -23,8 +23,7 @@ package «AdventOfCode» where
   moreServerArgs := moreServerArgs
   -- add any package configuration options here
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+require std from git "https://github.com/leanprover/std4" @ "main"
 
 @[default_target]
 lean_lib «AdventOfCode» where
